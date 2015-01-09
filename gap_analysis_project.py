93b9ef39481ecabb1884bf9e705affe151b6490e
Carlos Paredes <cparedes@covetel.com.ve>
2015-01-08 21:38:29 -0430

    Version 7.0 from Odoo page
    Signed-off-by: Carlos Paredes <cparedes@covetel.com.ve>

--- gap_analysis_project.py
+++ gap_analysis_project.py
@@ -52,15 +52,24 @@ class gap_analysis(osv.Model):
             for gap_line in gap.gap_lines:
                 if gap_line.to_project and gap_line.keep:
                     time4dev  = 0
+                    time4tech = 0
+                    time4fct  = 0
+                    time4test = gap_line.testing or 0
                     
                     if gap_line.effort:
                         if gap_line.effort.unknown:
                             time4dev = gap_line.duration_wk
                         else:
                             time4dev = gap_line.effort.duration
+                        
+                    for workload in gap_line.workloads:
+                        if workload.type.category == "Technical Analysis":
+                            time4tech += workload.duration
+                        else:
+                            time4fct  += workload.duration
                     
                     # Create Tasks
-                    if time4dev > 0:
+                    if time4dev > 0 or time4tech > 0 or time4fct > 0 or time4test > 0:
                         maintask_vals = {
                             'name': gap_line.functionality.name[0:100],
                             'code_gap': gap_line.code or "",
@@ -68,7 +77,7 @@ class gap_analysis(osv.Model):
                             'notes':         ustr(gap_line.functionality.description or gap_line.functionality.name),
                             'partner_id':    partner_id,
                             'gap_category_id': gap_line.category and gap_line.category.id or False,
-                            'user_id': False,
+                            'user_id': gap.user_functional and gap.user_functional.id or False,
                             'gap_line_id': gap_line.id,
                             'to_report': True,
                             'org_planned_hours': 0,
@@ -78,25 +87,74 @@ class gap_analysis(osv.Model):
                         maintask_id = task_pool.create(cr, uid, maintask_vals, context=context)
                         maintask_id = [int(maintask_id)]
                     
-                    for workload in gap_line.workloads:
-                        if workload.duration > 0:
-                            workload_vals = { 
-                                'name' : gap_line.functionality.name[0:100] + " / " + workload.type.name[0:50],
-                                'code_gap' : gap_line.code or "",
-                                'project_id' : project_id,
-                                'notes': "",
-                                'partner_id':    partner_id,
-                                'gap_category_id': gap_line.category and gap_line.category.id or False,
-                                'user_id': False,
-                                'gap_line_id': gap_line.id,
-                                'to_report': True,
-                                'parent_ids':    [(6,0,maintask_id)],
-                                'org_planned_hours': workload.duration,
-                                'planned_hours': workload.duration,
-                                'remaining_hours': workload.duration,
-                            }
-                            task_pool.create(cr, uid, workload_vals, context=context)
+                    if time4test > 0:    
+                        task_vals4test = {
+                            'name': gap_line.functionality.name[0:100] + " [TEST]",
+                            'code_gap': gap_line.code or "",
+                            'project_id':    project_id,
+                            'notes':         ustr(gap_line.functionality.description or gap_line.functionality.name),
+                            'partner_id':    partner_id,
+                            'org_planned_hours': time4test,
+                            'planned_hours': time4test,
+                            'remaining_hours': time4test,
+                            'parent_ids':    [(6,0,maintask_id)],
+                            'gap_category_id': gap_line.category and gap_line.category.id or False,
+                            'user_id': gap.user_test and gap.user_test.id or False,
+                            'gap_line_id': gap_line.id,
+                        }
+                        task_pool.create(cr, uid, task_vals4test, context=context)
                     
+                    if time4dev > 0:
+                        task_vals4dev = {
+                            'name': gap_line.functionality.name[0:100] + " [DEV]",
+                            'code_gap': gap_line.code or "",
+                            'project_id':    project_id,
+                            'notes':         ustr(gap_line.functionality.description or gap_line.functionality.name),
+                            'partner_id':    partner_id,
+                            'org_planned_hours': time4dev,
+                            'planned_hours': time4dev,
+                            'remaining_hours': time4dev,
+                            'parent_ids':    [(6,0,maintask_id)],
+                            'gap_category_id': gap_line.category and gap_line.category.id or False,
+                            'user_id': gap.user_dev and gap.user_dev.id or False,
+                            'gap_line_id': gap_line.id,
+                        }
+                        task_pool.create(cr, uid, task_vals4dev, context=context)
+                    
+                    if time4tech > 0:
+                        task_vals4tech = {
+                            'name': gap_line.functionality.name[0:100] + " [TECH]",
+                            'code_gap': gap_line.code or "",
+                            'project_id':    project_id,
+                            'notes':         ustr(gap_line.functionality.description or gap_line.functionality.name),
+                            'partner_id':    partner_id,
+                            'org_planned_hours': time4tech,
+                            'planned_hours': time4tech,
+                            'remaining_hours': time4tech,
+                            'parent_ids':    [(6,0,maintask_id)],
+                            'gap_category_id': gap_line.category and gap_line.category.id or False,
+                            'user_id': gap.user_technical and gap.user_technical.id or False,
+                            'gap_line_id': gap_line.id,
+                        }
+                        task_pool.create(cr, uid, task_vals4tech, context=context)
+                    
+                    if time4fct > 0:
+                        task_vals4fct = {
+                            'name': gap_line.functionality.name[0:100] + " [FUNC]",
+                            'code_gap': gap_line.code or "",
+                            'project_id':    project_id,
+                            'notes':         ustr(gap_line.functionality.description or gap_line.functionality.name),
+                            'partner_id':    partner_id,
+                            'org_planned_hours': time4fct,
+                            'planned_hours': time4fct,
+                            'remaining_hours': time4fct,
+                            'parent_ids':    [(6,0,maintask_id)],
+                            'gap_category_id': gap_line.functionality.category and gap_line.functionality.category.id or False,
+                            'user_id': gap.user_functional and gap.user_functional.id or False,
+                            'gap_line_id': gap_line.id,
+                        }
+                        task_pool.create(cr, uid, task_vals4fct, context=context)
+        
         if project_id:
             return {
             'type': 'ir.actions.act_window',
